79c5b76b07217c9452a41faad6656fa95040d838
Manue <Manue@MANUELMORA>
2025-05-26 15:51:02 -0500

    ll

---
+++ src/libreria/libreria_total.java
@@ -0,0 +1,35 @@
+package libreria;
+
+import java.awt.Image;
+import java.util.Random;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class libreria_total {
+
+    private void imagen_botton(JButton btn, String ruta) {//hay que cargar las imagenes desde cada boton 
+        ImageIcon img = new ImageIcon(ruta);
+        Image imgEscalada = img.getImage().getScaledInstance(btn.getWidth(), btn.getHeight(), Image.SCALE_SMOOTH);
+        btn.setIcon(new ImageIcon(imgEscalada));
+    }
+
+    
+
+    public void imagen_Label(JLabel labelname, String ruta) {
+        ImageIcon img = new ImageIcon(ruta);
+        Icon icon = new ImageIcon(img.getImage().getScaledInstance(labelname.getWidth(), labelname.getHeight(), 0));
+        labelname.setIcon(icon);
+    }
+
+    public static int[] llenarVector(int n) {
+        int vector[] = new int[n];
+        Random x = new Random();
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = x.nextInt(50) + 1; // Genera un número entre 1 y 50
+        }
+        return vector;
+    }
+}
